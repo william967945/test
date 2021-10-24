13bb9ea5a3921a3946a0b2594d62c8a3102d601c
william967945 <william967945@yahoo.com>
2021-10-24 15:14:33 +0800

    新增weather_app, delete png

---
+++ weather_app/weather_app_flutter-master/android/app/src/main/kotlin/com/example/weather_app_flutter/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.weather_app_flutter
+// MainActivity.kt
+-import android.os.Bundle
+-import io.flutter.app.FlutterActivity
++import io.flutter.embedding.android.FlutterActivity
+-import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  -  override fun onCreate(savedInstanceState: Bundle?) {
+    -    super.onCreate(savedInstanceState)
+    -    GeneratedPluginRegistrant.registerWith(this)
+    -  }
+}
